f32fccae1ed5315e68f3a5000fa1f07ada368b8a
zamashangase <1664818@students.wits.ac.za>
2022-04-12 00:05:15 +0200

    javascript
    javascript for website

---
+++ main.js
@@ -0,0 +1,63 @@
+ts<!DOCTYPE ;html>
+<html>
+<head>
+<script>
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Paragraph changed."
+    }
+    function myFunction() {
+        document.getElementById("demo").innerHTML = "Paragraph changed."
+    }
+    function InternetArt(Webpage) {
+        console.log('MyInternetArt' + website)
+    }
+    <script>
+        src= "https://zamashangase.github.io/WSOA4175A_1664818/index.html/js/myScript.js"
+    </script>
+    <script src="main.js"></script>
+</script>
+</head>
+<body>
+    <h1>Welcome To My Internet Art Website</h1>
+    <p id= "demo"> This is a website for the information on internet art and the information on blog posts and a design section</p>
+    <button type= "button"
+    onclick="myFunction()"Try It></button>
+    
+    let c = 1.01 + 2.02;
+    console.log(c.toString));
+    console.log(c.toString));
+
+</body>
+</html>
+//code for Responsive design javascript//
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+/*toggle mobile menu or mobile function*/
+function toggleMenu() {
+    if(menu.classList.contains("active")){
+        menu.classList.remove("active");
+        toggle.querySelector("a").innerHTML = "< class='fas fa-times'></i>";
+    }else{
+        menu.classList.add("active");
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "< class='fas fa-times'></i>";
+    }
+}
+
+fetch("https://zamashangase.github.io/WSOA4175A_1664818/index.html")
+.then((r) => r.json())
+.then((nandi) => {
+    console.log(myinternetArtGallery);
+    console.log('zama reveived');
+    handleMyInternetArt(myinternetArtGallery);
+})
+.catch((error) => console.warn("Our Warning", error));
+
+const handleMyInternetArt = (myinternetArtGallery) => {
+    document.querySelector("p").innerText = zamaInternetArt.value;
+    document.querySelector("img").src = myinternetArtGallery.img1_url;
+    console.log("This website was created by Zamashuku");
+
+    
+};
+console.log('This webpage was created by Zama to dicuss internet art')
